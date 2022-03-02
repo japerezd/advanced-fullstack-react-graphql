3c129a96385f7447379e28f8d6a0947a445d6c6f
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-02 08:41:10 -0600

    Configure nprogress

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,13 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 // this is our layout page
 export default function MyApp({ Component, pageProps }) {
   return (
