05be39c3be807ebf318e0d368457156c97309f05
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-02 08:53:55 -0600

    Fix styled components error

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,6 +1,15 @@
 import Document, { Html, NextScript, Main, Head } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
   render() {
     return (
       <Html lang="es-MX">
