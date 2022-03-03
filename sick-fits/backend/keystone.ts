99992176f6da5e3798fd7f990e166d3939bc8a8f
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-03 14:41:43 -0600

    Create ProductImage type and upload an image

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -43,6 +44,7 @@ export default withAuth(
       // Schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // Show the UI only for people who pass this test
