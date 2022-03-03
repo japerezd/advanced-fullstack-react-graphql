fba1bba9cf03a4c7fd1aef3b6420a0a5bc2f126a
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-03 08:58:59 -0600

    Create Product type

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -6,6 +6,7 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -41,6 +42,7 @@ export default withAuth(
     lists: createSchema({
       // Schema items go in here
       User,
+      Product,
     }),
     ui: {
       // Show the UI only for people who pass this test
---
+++ sick-fits/backend/schemas/Product.ts
@@ -0,0 +1,29 @@
+import { list } from '@keystone-next/keystone/schema';
+import { text, relationship, select, integer } from '@keystone-next/fields';
+
+export const Product = list({
+  // todo:
+  // access
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+    // TODO: photo
+  },
+});
