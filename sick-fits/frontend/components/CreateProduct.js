3951963166a8b8d5cf1125fd5ff705248608edb6
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-05 12:27:52 -0600

    Create useForm custom hook

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import useForm from '../lib/useForm';
 
 export default function CreateProduct() {
-  const [name, setName] = useState('Beto');
+  const { inputs, handleChange, clearForm, resetForm } = useForm({});
   return (
     <form>
       <label htmlFor="name">
@@ -11,9 +11,30 @@ export default function CreateProduct() {
           id="name"
           name="name"
           placeholder="Name"
-          value={name}
+          value={inputs.name}
+          onChange={handleChange}
         />
       </label>
+
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear form
+      </button>
+
+      <button type="button" onClick={resetForm}>
+        Reset form
+      </button>
     </form>
   );
 }
---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // Create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  // {
+  //   name: 'bet',
+  //   description: 'this is a ...',
+  //   price: 1000
+  // }
+
+  function handleChange(e) {
+    let { name, type, value } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+    setInputs({
+      // Copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
