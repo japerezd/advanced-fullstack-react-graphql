b696ca171b19ee944d4905efa56a3a719ac549dc
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-08 08:37:54 -0600

    Add query and useUser custom hook to query the current user

---
+++ sick-fits/frontend/components/User.js
@@ -0,0 +1,21 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+export const CURRENT_USER_QUERY = gql`
+  query {
+    authenticatedItem {
+      # We are making... on, because authenticatedItem return an union
+      ... on User {
+        id
+        email
+        name
+        # TODO: query the cart once we have it
+      }
+    }
+  }
+`;
+
+export function useUser() {
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+}
