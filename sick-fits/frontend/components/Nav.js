300b7a6e1e37b7a3e41704475d0288c9aad45b30
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-09 15:33:04 -0600

    Create SignOut component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import SignOut from './SignOut';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 
@@ -12,6 +13,7 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
 
