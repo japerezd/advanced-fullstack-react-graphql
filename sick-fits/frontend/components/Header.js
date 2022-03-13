631114d8ed8b11f7b58c959105bee39225abbeb6
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-13 17:15:13 -0600

    :sparkles: Display cart items in custom component

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import Cart from './Cart';
 import Nav from './Nav';
 
 const Logo = styled.h1`
@@ -46,6 +47,8 @@ export default function Header() {
       <div className="sub-bar">
         <p>Search</p>
       </div>
+
+      <Cart />
     </HeaderStyles>
   );
 }
