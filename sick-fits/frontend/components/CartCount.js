bcf5807e0d76b7f4ab4999ac3876d3126f962926
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-13 20:02:32 -0600

    :sparkles: Add CartCount component and animate the cart value

---
+++ sick-fits/frontend/components/CartCount.js
@@ -0,0 +1,60 @@
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+
+const Dot = styled.div`
+  background: var(--red);
+  color: #fff;
+  border-radius: 50%;
+  padding: 0.5rem;
+  line-height: 2;
+  min-width: 3rem;
+  margin-left: 1rem;
+  font-feature-settings: 'tnum';
+  font-variant-numeric: tabular-nums;
+`;
+
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    ${'' /* The amount of time should be the same as the timeout */}
+    transition: transform 0.4s;
+    backface-visibility: hidden;
+  }
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
+
+export default function CartCount({ count }) {
+  return (
+    <AnimationStyles>
+      <TransitionGroup>
+        <CSSTransition
+          unmountOnExit
+          className="count"
+          classNames="count"
+          key={count}
+          timeout={{ enter: 400, exit: 400 }}
+        >
+          <Dot>{count}</Dot>
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyles>
+  );
+}
