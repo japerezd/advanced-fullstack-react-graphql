3c6cdbef80b46cfbbd17c71b9529c77a0840f93c
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-03-13 20:44:06 -0600

    :sparkles: Remove item from cache

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -20,9 +20,22 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  // To delete item from cache
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
+    update,
+    // optimisticResponse: {
+    //   // this is useful when we know what we are gonna take from backend
+    //   deleteCartItem: {
+    //     __typename: 'CartItem',
+    //     id,
+    //   },
+    // },
   });
   return (
     <BigButton
